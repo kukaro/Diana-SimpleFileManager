270a5aff6d24fc6f42e209cbcffd0c6ef3bffd0d
DESKTOP-JS8DOEJ\jiharu <justkukaro@naver.com>
2018-01-18 00:44:14 +0900

    v0.1.4 complete header and nav solution

--- public/js/diana.js
+++ public/js/diana.js
@@ -1,24 +1,93 @@
 var file = {
-    alpha: {'type': 'folder', 'child': {gamma: {'type': 'folder', 'child': null, 'data': null}}, 'data': null},
-    beta: {'type': 'image'}
+    type: 'folder', child:
+        {
+            alpha: {type: 'folder', child: {gamma: {type: 'folder', child: null, data: null}}, data: null},
+            beta: {type: 'image'}
+        },
+    data: null
 };
+var currentPathToList = ['/'];
+var fileIndex = 1;
+var currentFile = file;
 $(document).ready(function () {
-    $('.file-manager-header').append(makeDiv());
-    for (var item in file) {
-        $('.file-manager-header').append(makeDiv(item));
-    }
+    $('.file-manager-header').append(makeHeaderDiv());
 });
 
-function makeDiv(str) {
+function makeHeaderDiv(str) {
+    if (str == undefined) {
+        str = '/';
+    }
+    return '<button type="button" class="btn btn-info btn-outline-primary file-manager-button" onclick="fileManagerNavUpdateToHeader(\'' + str + '\',' + (fileIndex++) + ')">' + str + '</button>';
+}
+
+function makeNavDiv(str) {
     if (str == undefined) {
         str = '';
     }
-    return '<button type="button" class="btn btn-info btn-outline-primary file-manager-button" onclick="fileManageNavUpdate(\'' + str + '\')">' + str + '/</button>';
+    return '<button type="button" class="btn btn-info btn-outline-primary file-manager-nav-button" onclick="fileManagerNavUpdateToNav(\'' + str + '\')">' + str + '</button>';
 }
 
-function fileManageNavUpdate(str) {
-    if (str == '' || undefined) {
+function fileManagerNavUpdateToHeader(str, num) {
+    if (str == '' || undefined || str == '/') {
+        str = '/';
+        var count = currentPathToList.length;
+        for (var i = num + 1; i <= count; i++) {
+            currentPathToList.pop();
+        }
+        console.log(currentPathToList);
+        currentFile = file;
+        $('.file-manager-header').html('');
+        fileIndex = 1;
+        for (var i = 0; i < currentPathToList.length; i++) {
+            if (currentPathToList[i] == '/') {
+                $('.file-manager-header').append(makeHeaderDiv());
+            }
+            else {
+                $('.file-manager-header').append(makeHeaderDiv(currentPathToList[i]));
+            }
+            if (currentPathToList.length - 1 != i) {
+                currentFile = currentFile['child'][currentPathToList[i + 1]];
+            }
+        }
+    }
+    else {
+        for (var i = num + 1; i <= currentPathToList.length; i++) {
+            currentPathToList.pop();
+        }
+        currentFile = file;
+        $('.file-manager-header').html('');
+        fileIndex = 1;
+        for (var i = 0; i < currentPathToList.length; i++) {
+            if (currentPathToList[i] == '/') {
+                $('.file-manager-header').append(makeHeaderDiv());
+            }
+            else {
+                $('.file-manager-header').append(makeHeaderDiv(currentPathToList[i]));
+            }
+            if (currentPathToList.length - 1 != i) {
+                currentFile = currentFile['child'][currentPathToList[i + 1]];
+            }
+        }
+    }
+    $('.file-manager-nav-title-value').html(str);
+    $('.file-manager-nav-body').html('');
+    for (var item in currentFile['child']) {
+        $('.file-manager-nav-body').append(makeNavDiv(item));
+    }
+}
+
+function fileManagerNavUpdateToNav(str) {
+    if (str == '' || undefined || str == '/') {
         str = '/';
     }
-    $('.file-manager-nav-title').html(str);
+    else {
+        currentPathToList.push(str);
+        $('.file-manager-header').append(makeHeaderDiv(str));
+        currentFile = currentFile['child'][str];
+    }
+    $('.file-manager-nav-title-value').html(str);
+    $('.file-manager-nav-body').html('');
+    for (var item in currentFile['child']) {
+        $('.file-manager-nav-body').append(makeNavDiv(item));
+    }
 }
